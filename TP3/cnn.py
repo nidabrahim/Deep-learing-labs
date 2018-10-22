9538a5b116d22c11ec70c8d85aefee23850d7038
Youssef  NIDABRAHIM <youssef.nidabrahim@etu.uca.fr>
2018-10-22 15:54:44 +0200

    TP3

--- TP3/cnn.py
+++ TP3/cnn.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 ''' 
@@ -20,11 +21,12 @@ Données MNIST
 
 # Import MNIST data
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("../../data/mnistdata", one_hot=True)
+mnist = input_data.read_data_sets("../data/mnistdata", one_hot=True)
 
 num_examples = mnist.train.images.shape[0] 
 num_input = mnist.train.images.shape[1]
 num_classes = mnist.train.labels.shape[1]
+
 
 
 # Affichage des informations par pas de temps
@@ -43,7 +45,7 @@ Convolution 2D avec RELU
 def conv2d(x, W, b, strides=1):
     x = tf.nn.conv2d(x, W, strides=[1, strides, strides, 1], padding='SAME')
     x = tf.nn.bias_add(x, b)
-    return tf.nn.relu(x);
+    return tf.nn.relu(x)
 
 '''
 -------------------------------------------------------------------------
@@ -51,8 +53,7 @@ Pooling max
 -------------------------------------------------------------------------
 ''' 
 def maxpool2d(x, k=2):
-    return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1], padding='SAME')
-
+    return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1],padding='SAME')
 
 '''
 -------------------------------------------------------------------------
@@ -64,48 +65,44 @@ def conv_net(x, poids, biais, dropout):
     # Mise en forme de l'image d'entrée
     x = tf.reshape(x, shape=[-1, 28, 28, 1])
 
-    conv1 = conv2d(x, W=poids['wconv1'], b=biais['bconv1'], strides=1)
+    conv1 = conv2d(x, poids['wconv1'], biais['bconv1'])
     conv1 = maxpool2d(conv1, k=2)
-    
-    conv2 = conv2d(conv1, W=poids['wconv2'], b=biais['bconv2'], strides=1)
+
+    conv2 = conv2d(conv1, poids['wconv2'], biais['bconv2'])
     conv2 = maxpool2d(conv2, k=2)
 
-    # Mise en forme des activations de la seconde couche cachée pour l'entrée de la couche complètement connectée
+    # Mise en forme des activations de la seconde couche cachée pour l'entrée de 
+    # la couche complètement connectée
     fcl = tf.reshape(conv2, [-1, poids['wfcl'].get_shape().as_list()[0]])
-    fcl = tf.add(tf.matmul(fcl, poids['fcl']), biais['bfcl'])
+    fcl = tf.add(tf.matmul(fcl, poids['wfcl']), biais['bfcl'])
     fcl = tf.nn.relu(fcl)
     
-    # Dropout 
-    fcl = tf.nn.dropout(fcl, dropout)
+    # Dropout
+    fcl_do = tf.nn.dropout(fcl, dropout)
 
     # Couche de sortie
-    out = tf.add(tf.matmul(fcl, poids['out']), biais['out'])
+    out = tf.add(tf.matmul(fcl_do, poids['out']), biais['out'])
 
     return out
 
-
-
 #Stockage des poids et biais dans des variables TF
-'''
-TODO : initialiser avec une loi normale des variables tensorFlow :
-    - wconv1 et bcconv1 pour CONV1. wconv1 est un banc de 32 filtres 5*5*1. bconv1 a une taille adaptée
-    - wconv2 et bconv2 pour CONV2. wcconv2 est un banc de 32 filtres 5*5*32*64. bconv2 a une taille adaptée
-    - wfcl et bfcl pour la couche complètement connectée : wfcl est un banc de filtres 7*7*64 à 1024 sorties. bfcl a une taille adaptée
-    - out (poids et biais) ont une taille adaptée pour la classification des données MNIST
-'''
 poids = {
+    # CONV1 : noyaux 5x5 , 1 entrée, 32 sorties
     'wconv1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
+    # CONV2 : noyaux5x5 conv, 32 entrées, 64 sorties
     'wconv2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
+    # FCL 7*7*64 entrées, 1024 sorties
     'wfcl': tf.Variable(tf.random_normal([7*7*64, 1024])),
-    'out': tf.Variable(tf.random_normal([num_hidden_2, num_classes]))
+    # Prédiction : 1024 entrées, 10 sorties (class prediction)
+    'out': tf.Variable(tf.random_normal([1024, num_classes]))
 }
+
 biais = {
-    'bconv1': tf.Variable(tf.random_normal([num_hidden_1])),
-    'bconv2': tf.Variable(tf.random_normal([num_hidden_1])),
-    'bfcl': tf.Variable(tf.random_normal([num_hidden_2])),
+    'bconv1': tf.Variable(tf.random_normal([32])),
+    'bconv2': tf.Variable(tf.random_normal([64])),
+    'bfcl': tf.Variable(tf.random_normal([1024])),
     'out': tf.Variable(tf.random_normal([num_classes]))
 }
-
 
 
 # Construction du modèle
@@ -113,9 +110,8 @@ pred = conv_net(x, poids, biais, keep_prob)
 
 
 # Fonction de perte et procédure d'optimisation
-#TODO : entropie croisée avec logits, algorithme d'optimisation ADAM
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=pred)
-loss_operation = tf.reduce_mean(cross_entropy)
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
 
 # Evaluation du modèle
@@ -147,5 +143,18 @@ with tf.Session() as sess:
                 print("Iteration " + str(epoch * total_batch + step) + ", Précision  = " + "{:.5f}".format(acc))
 
     # Test
-   
     print("Test:", sess.run(accuracy, feed_dict={x: mnist.test.images[:256],y: mnist.test.labels[:256],keep_prob: 1.}))
+
+    # Visualisation des 5 premiers filtres de chaque couche
+    filters = poids['wconv1'].eval()
+    filters2 = poids['wconv2'].eval()
+    fig = plt . figure ()
+    for i in range(10):
+    	ax = fig.add_subplot(4, 5, i+1)
+    	ax.matshow(filters[: ,: ,0 , i ] , cmap='gray')
+    	ax = fig.add_subplot(4, 5, 10+i+1)
+    	ax.matshow(filters2[: ,: ,0 , i ] , cmap='gray')
+    plt.suptitle('Dix premiers filtres (conv1 ligne 1, conv2 ligne 2)')
+    fig.savefig('filters.png')
+    
+
